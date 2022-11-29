0133dd1a2997064998c072eece7d8ffab4d26959
tauseefRazaq <i211236@nu.edu.pk>
2022-11-30 01:13:54 +0500

    linked list for student data is created

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 
 int main(){
     StudentList l1;
-    l1.addStudent(0, "Tauseef", "02-11-2001", "1-1-2007", "Chakwal", "FSc", 'M');
-    l1.addStudent(1, "Irfa", "18-04-2002", "1-2-2008", "Chakwal", "FSc", 'F');
+    // l1.addStudent(0, "Tauseef", "02-11-2001", "1-1-2007", "Chakwal", "FSc", 'M');
     readData(l1);
     l1.displayList();
  return EXIT_SUCCESS;
